96012aeb8460cdd46229830d4e003d0a0df538d9
Jonas Gao <gaopinsong@live.cn>
2024-07-24 19:42:48 +0800

    Update ubuntu aliases.

--- ubuntu/install-tools.sh
+++ ubuntu/install-tools.sh
@@ -2,7 +2,9 @@
 sudo apt install git fd-find bat-cat ripgrep openssh-server openssh-client python3-neovim
 mkdir "$HOME/.config/"
 git clone https://github.com/JonasGao/nvimrc.git $HOME/.config/nvim
-printf """alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
+printf """alias fd='fdfind'
+alias bat='batcat'
+alias n='nvim'
+alias k='kubectl'
+alias h='helm'
 """ >"$HOME/.bash_aliases"
