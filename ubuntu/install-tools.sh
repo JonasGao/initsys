f623a6d29c7923a7cf39d804d714f7ca8c87eea3
Jonas Gao <gaopinsong@live.cn>
2024-07-24 19:53:01 +0800

    Update ubuntu neovim installation

--- ubuntu/install-tools.sh
+++ ubuntu/install-tools.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo apt install git fzf fd-find bat-cat ripgrep openssh-server openssh-client python3-neovim
+sudo apt install git fzf fd-find bat-cat ripgrep openssh-server openssh-client
+sudo add-apt-repository ppa:neovim-ppa/stable -y
+sudo apt update
+sudo apt install neovim
 mkdir "$HOME/.config/"
 git clone https://github.com/JonasGao/nvimrc.git $HOME/.config/nvim
 printf """alias fd='fdfind'
